b388cc18fff79a5bd3a2d5a5308889c90fd2ee77
JonFerSanLuis <jonfernandez.sanluis@gmail.com>
2026-05-12 22:18:41 +0200

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,21 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
+        // 1. Primero llamamos al seeder de roles (que creamos antes)
         $this->call(RoleSeeder::class);
+
+        // 2. Creamos al usuario Administrador por defecto
+        \App\Models\User::create([
+            'name' => 'Jon Admin',
+            'email' => 'admin@venue.com',
+            'password' => Hash::make('12345678'), // Encriptamos la contraseña
+            'role_id' => 1, // Le asignamos el rol de admin directamente
+        ]);
     }
 }
